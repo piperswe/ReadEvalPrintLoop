09a3bf0a823ab751cec71791401c176e9664dde1
Piper McCorkle <contact@piperswe.me>
2025-12-04 17:44:37 -0600

    Initial Commit

---
+++ ReadEvalPrintLoop/ReadEvalPrintLoopApp.swift
@@ -0,0 +1,32 @@
+//
+//  ReadEvalPrintLoopApp.swift
+//  ReadEvalPrintLoop
+//
+//  Created by Piper McCorkle on 12/4/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ReadEvalPrintLoopApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ ReadEvalPrintLoopTests/ReadEvalPrintLoopTests.swift
@@ -0,0 +1,16 @@
+//
+//  ReadEvalPrintLoopTests.swift
+//  ReadEvalPrintLoopTests
+//
+//  Created by Piper McCorkle on 12/4/25.
+//
+
+import Testing
+
+struct ReadEvalPrintLoopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
