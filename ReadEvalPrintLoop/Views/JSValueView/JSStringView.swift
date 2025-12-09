812f4be7ce7e7f4a6d8e98c3c5d0f7d35b99f2eb
Piper McCorkle <contact@piperswe.me>
2025-12-08 19:26:16 -0600

    allow copying strings

--- ReadEvalPrintLoop/Views/JSValueView/JSStringView.swift
+++ ReadEvalPrintLoop/Views/JSValueView/JSStringView.swift
@@ -8,17 +8,39 @@
 //  All rights reserved.
 //
 
+import AlertToast
 import SwiftUI
+
+#if os(macOS)
+  import AppKit
+#else
+  import UIKit
+#endif
 
 struct JSStringView: JSValueViewBase {
   var value: String
   var logMessage: Bool = false
+  @State var showToast: Bool = false
   var body: some View {
     HStack(alignment: .top) {
       if !logMessage {
         icon(name: "text.document", type: "string")
       }
       Text("\(value)")
+      Button("Copy", systemImage: "document.on.document") {
+        #if os(macOS)
+          let pasteboard = NSPasteboard.general
+          pasteboard.setString(value, forType: .string)
+        #else
+          let pasteboard = UIPasteboard.general
+          pasteboard.clearContents()
+          pasteboard.writeObjects([value as NSString])
+        #endif
+        showToast = true
+      }.controlSize(.mini)
+    }
+    .toast(isPresenting: $showToast) {
+      AlertToast(displayMode: .hud, type: .regular, title: "Copied!")
     }
   }
 }
